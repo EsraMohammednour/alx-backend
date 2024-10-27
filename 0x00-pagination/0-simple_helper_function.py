db24102d4c763fa7f86396236f2c839c29f4f7c8
esraa <esraa2023112@gmail.com>
2024-10-27 12:11:52 +0300

    done

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,8 +4,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    '''Function that return pagination parameter'''
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
-
+    '''Function that bring page number'''
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
