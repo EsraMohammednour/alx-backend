34c17b6a6241504fde7c5b7f32a31b63f13e8beb
esraa <esraa2023112@gmail.com>
2024-10-30 22:38:51 +0300

    done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,7 +18,7 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_key = self.cache_data.popitem(False)
-            print(f"DISCARD:", first_key)
+            print("DISCARD:", first_key)
 
     def get(self, key):
         '''return the value from cache_data'''
