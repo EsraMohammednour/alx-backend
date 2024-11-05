8c5b26154c0f824bd40671fbead5567312694d99
esraa <esraa2023112@gmail.com>
2024-11-05 21:40:13 +0200

    done

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Module thatconfigure available languages in our app'''
+ from flask import Flask, render_template
+ from flask_babel import Babel
+
+
+app = Flask(__name__)
+class Config:
+    '''class that using for set lang'''
+    LANGUAGES = ["en", "fr"]
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+babel = Babel(app)
+@app.route('/')
+def index():
+    '''display page'''
+    return render_template('1-index.html')
